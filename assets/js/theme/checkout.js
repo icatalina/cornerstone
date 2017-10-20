985332ad50bf0edcf690926055970461aeb30d41
David Chin <davidchin.yf@gmail.com>
2017-10-20 17:30:47 +1100

    Import ng-checkout

--- assets/js/theme/checkout.js
+++ assets/js/theme/checkout.js
@@ -1,8 +1,14 @@
 import PageManager from './page-manager';
+import { initializeSdk } from 'ng-checkout/dist/checkout-sdk';
 
 export default class Checkout extends PageManager {
     loaded(next) {
-        console.log('Loaded checkout');
+        const { checkoutService } = initializeSdk();
+
+        checkoutService.loadQuote()
+            .then(({ checkout }) => {
+                console.log(checkout.getCart());
+            });
 
         next();
     }
